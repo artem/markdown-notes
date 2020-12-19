3534e65de2e09cc8c3b92b94e3d67c3c2e319e5e
Artem Labazov <123321artyom@gmail.com>
2020-12-19 03:46:47 +0300

    Use textarea

--- src/jsMain/kotlin/welcome.kt
+++ src/jsMain/kotlin/welcome.kt
@@ -1,17 +1,20 @@
+import kotlinx.browser.document
+import kotlinx.html.ButtonType
 import kotlinx.html.InputType
+import kotlinx.html.id
 import kotlinx.html.js.onChangeFunction
+import kotlinx.html.js.onClickFunction
 import org.intellij.markdown.flavours.MarkdownFlavourDescriptor
 import org.intellij.markdown.flavours.commonmark.CommonMarkFlavourDescriptor
 import org.intellij.markdown.html.HtmlGenerator
 import org.intellij.markdown.parser.MarkdownParser
 import org.w3c.dom.HTMLInputElement
+import org.w3c.dom.HTMLTextAreaElement
 import react.RBuilder
 import react.RComponent
 import react.RProps
 import react.RState
-import react.dom.InnerHTML
-import react.dom.div
-import react.dom.input
+import react.dom.*
 
 external interface WelcomeProps : RProps {
     var name: String
@@ -23,25 +26,32 @@ data class WelcomeState(val name: String, val compiled: String) : RState
 class Welcome(props: WelcomeProps) : RComponent<WelcomeProps, WelcomeState>(props) {
 
     init {
-        state = WelcomeState(props.name, "ok")
+        state = WelcomeState(props.name, props.name)
     }
 
     override fun RBuilder.render() {
         div {
-            attrs["dangerouslySetInnerHTML"] = InnerHTML("Hello, ${state.compiled}")
+            attrs["dangerouslySetInnerHTML"] = InnerHTML(state.compiled)
             //+"Hello, ${state.compiled}"
         }
-        input {
+        textArea {
             attrs {
-                type = InputType.text
-                value = state.name
-                onChangeFunction = { event ->
-                    val typed = (event.target as HTMLInputElement).value
+                id = "note-content"
+                +state.name
+            }
+
+        }
+
+        button {
+            attrs {
+                onClickFunction = { _ ->
+                    val typed = (document.getElementById("note-content") as HTMLTextAreaElement).value
                     setState(
-                            WelcomeState(name = typed, compiled = parseMd(typed))
+                        WelcomeState(name = typed, compiled = parseMd(typed))
                     )
                 }
             }
+            +"Preview"
         }
     }
 }
