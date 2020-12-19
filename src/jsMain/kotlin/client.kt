af5321aeb733bec94be2a49e7758ba899a4de4fc
Artem Labazov <123321artyom@gmail.com>
2020-12-20 00:54:27 +0300

    Render as soon as possible

--- src/jsMain/kotlin/client.kt
+++ src/jsMain/kotlin/client.kt
@@ -1,29 +1,26 @@
 import kotlinx.browser.document
-import kotlinx.browser.window
 import kotlinx.datetime.Clock
 import react.dom.h3
 import react.dom.render
 
 fun main() {
-    window.onload = {
-        render(document.getElementById("root")) {
-            child(Welcome::class) {
-                attrs {
-                    name = "Kotlin/JS"
-                }
+    render(document.getElementById("root")) {
+        child(Welcome::class) {
+            attrs {
+                name = "Kotlin/JS"
             }
-            h3 {
-                +"All notes:"
-            }
-            child(MarkdownList::class) {
-                attrs {
-                    list = listOf(
-                        NoteMeta(1, "first note", Clock.System.now()),
-                        NoteMeta(2, "kek", Clock.System.now()),
-                        NoteMeta(9, "first note", Clock.System.now()),
-                        NoteMeta(10, "Hello w", Clock.System.now()),
-                    )
-                }
+        }
+        h3 {
+            +"All notes:"
+        }
+        child(MarkdownList::class) {
+            attrs {
+                list = listOf(
+                    NoteMeta(1, "first note", Clock.System.now()),
+                    NoteMeta(2, "kek", Clock.System.now()),
+                    NoteMeta(9, "first note", Clock.System.now()),
+                    NoteMeta(10, "Hello w", Clock.System.now()),
+                )
             }
         }
     }
